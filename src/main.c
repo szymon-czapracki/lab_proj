af42e719c7966c261f55f6fa95dba11f76cc78d3
Szymon Czapracki <szymonczapracki@gmail.com>
2021-06-02 02:12:11 +0200

    Blueprint for final main functionality

--- src/main.c
+++ src/main.c
@@ -3,47 +3,74 @@
 #include <time.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include "../include/student.h"
 #include "../include/student_list.h"
 
 int main()
 {
+	char *file_option = (char*) malloc(10 * sizeof(char));
+	int num_of_students, decider;
+	student *student_array;
+	char *gen = "gen";
+	char *load = "load";
+
 	srand(time(NULL) + getpid());
-	int num_of_students;
-	int sort_method;
-	int search;
 
+	printf("Do you want to generate new database or load from file"
+			"[gen/load]: ");
+	scanf("%s", file_option);
 
-	printf("Enter number of students\n");
-	scanf("%d", &num_of_students);
+	while (!strcmp(file_option, gen) || !strcmp(file_option, load)) {
+		scanf("%s", file_option);
+	}
 
-	while (num_of_students < 0) {
+	if (strcmp(file_option, gen)) {
+		printf("\nHow many students in database?\n");
 		scanf("%d", &num_of_students);
+		while (num_of_students <= 0) {
+			scanf("%d", &num_of_students);
+		}
+		student_array = generate_student_list(num_of_students);
+	} else if (strcmp(file_option, load)) {
+		// TODO add option to load from file
 	}
 
-	student *student_array = malloc(num_of_students * sizeof(*student_array));
 
-	for (int i = 0; i < num_of_students; i++) {
-		student_array[i] = generate_student();
+	while (decider != 0) {
+		printf("\nWhich function would you want to tests?\n");
+		printf("1 - Test printing database onto screen\n");
+		printf("2 - Test finding individual student\n");
+		printf("3 - Test database sorting\n");
+		printf("4 - Test changing student info\n");
+		printf("5 - Test saving database to file\n");
+		printf("6 - Test adding student by user input\n");
+		printf("0 - Exit\n");
+		scanf("%d", &decider);
+
+		switch (decider) {
+			case 1:
+				print_student_list(student_array,
+							num_of_students);
+				break;
+			case 2:
+				// TODO
+				break;
+			case 3:
+				// TODO
+				break;
+			case 4:
+				// TODO
+				break;
+			case 5:
+				// TODO
+				break;
+			case 6:
+				// TODO
+				break;
+			default:
+				break;
+		}
 	}
-
-	print_student_list(student_array, num_of_students);
-
-	//scanf("%d", &sort_method);
-	//while (sort_method < 1 || sort_method > 4) {
-	//	scanf("%d", &num_of_students);
-	//}
-
-	printf("Enter serial_number\n");
-	scanf("%d", &search);
-
-	print_student(find_student_by_serial_number(student_array,
-						num_of_students, search));
-
-	//for (int i = 0; i < num_of_students; i++) {
-	//	printf("%-3d ", i);
-	//	print_student(student_array[i]);
-	//}
-
 	return (0);
 }
